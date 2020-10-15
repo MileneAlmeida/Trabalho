0d15c9a6efa09fbb0019f2d48c9c9fe97aead0dc
MileneAlmeida <30482022+MileneAlmeida@users.noreply.github.com>
2020-10-15 17:57:11 -0300

    Add files via upload

---
+++ reserva.java
@@ -0,0 +1,33 @@
+
+package aplicação;
+
+
+public class calculo {
+    private int valordescont;
+    private int valortotal;
+    
+ plublic int calculardesconto (int valor, int idade){
+     {
+        int valordesconto = valor*10/100;
+        
+ }
+     else {
+        int valordesconto = valor*1/100;
+        
+    }
+        int valordesconto = 0;
+     return valordesconto;
+ }
+ public int calculartotal(int valor, int desconto){
+     valortotal = valor-desconto;
+     return valortotal;
+  
+}
+
+    private static class plublic {
+
+        public plublic() {
+        }
+    }
+ 
+}
